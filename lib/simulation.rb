6163ff7fdddae9895568933e7d9950db7a36a8a7
Jaxon Avena <jaxonavena@gmail.com>
2023-08-10 13:58:02 -0500

    Refactor Switch method

--- lib/simulation.rb
+++ lib/simulation.rb
@@ -37,9 +37,9 @@ class Simulation
 
   def announce_teams
     puts "\nTEAM 1:"
-    @team1.pokemon.each { |pokemon| puts pokemon.name }
+    @team1.each { |pokemon| puts pokemon.name }
     puts "\nTEAM 2:"
-    @team2.pokemon.each { |pokemon| puts pokemon.name }
+    @team2.each { |pokemon| puts pokemon.name }
   end
 
   def set_opponents
